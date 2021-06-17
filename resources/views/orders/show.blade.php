04768a72bef66fdcb9ef569340a4ecfc7b57065c
Gyh592358736 <77520082@qq.com>
2021-06-17 16:33:27 +0800

    后台管理 - 同意退款 - 支付宝退款

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -90,11 +90,18 @@
                                 未支付
                             @endif
                         @endif
+                      
                         <!-- 如果订单的发货状态为已发货则展示确认收货按钮 -->
                         @if($order->ship_status === \App\Models\Order::SHIP_STATUS_DELIVERED)
                         <div class="receive-button">
                            
                             <button type="button" id="btn-receive" class="btn btn-sm btn-success">确认收货</button>
+                        </div>
+                        @endif
+                        @if(isset($order->extra['refund_disagree_reason']))
+                        <div>
+                        <span>拒绝退款理由：</span>
+                        <div class="value">{{ $order->extra['refund_disagree_reason'] }}</div>
                         </div>
                         @endif
                         <!-- 订单已支付，且退款状态是未退款时展示申请退款按钮 -->
@@ -114,6 +121,7 @@
                         @endif
                         <!-- 支付按钮结束 -->
                     </div>
+                   
                 </div>
                 </div>
             </div>
